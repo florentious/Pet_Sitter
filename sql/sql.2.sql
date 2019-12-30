792a3497c9a7d48636683f0c4b4dea1d78efd90d
florentious <florentious09@gmail.com>
2019-12-30 18:39:24 +0900

    2019.12.30-v.1.1

--- sql/sql.2.sql
+++ sql/sql.2.sql
@@ -15,22 +15,6 @@ m_point_count INT
 
 );
 
-SELECT * FROM p_member;
-
-
-CREATE TABLE p_wanted(
-w_no INT PRIMARY KEY,
-w_title VARCHAR(50),
-w_content VARCHAR(1024),
-w_regDate DATETIME,
-isEnd BOOLEAN,
-w_id VARCHAR(30),
-foreign KEY (w_id) REFERENCES p_member(m_id)
-
-);
-
-SELECT * FROM p_wanted
-
 CREATE TABLE notice(
 
 n_no INT PRIMARY KEY,
@@ -41,4 +25,28 @@ n_contents VARCHAR(100) NOT null
 
 );
 
-SELECT * FROM notice
+
+CREATE TABLE p_wanted(
+w_no INT PRIMARY KEY,
+w_title VARCHAR(50),
+w_content VARCHAR(1024),
+w_regDate DATETIME,
+w_isEnd BOOLEAN,
+w_id VARCHAR(30),
+foreign KEY (w_id) REFERENCES p_member(m_id)
+
+);
+
+CREATE TABLE p_point_date(
+p_sitter_id VARCHAR(30),
+p_applic_id VARCHAR(30),
+p_regDate DATETIME
+
+
+)
+
+CREATE TABLE p_booking
+
+
+SELECT * FROM p_member
+
